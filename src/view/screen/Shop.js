16dd670a7e583724e6f5f86669bfc35e64db6421
hanuman12511 <er.hanumansaini@gmail.com>
2024-04-06 13:43:14 +0530

    nav

--- src/view/screen/Shop.js
+++ src/view/screen/Shop.js
@@ -2,14 +2,17 @@ import { Col, Container, Row } from "react-bootstrap"
 import {shop} from '../data/data'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useNavigate } from "react-router-dom";
+
 function Shop(){
+    const nav =useNavigate()
     return(
         <>
         <Container>
             <Row>
                 {
                     shop.map(value=>(
-                        <Col lg={3} md={6} sm={6}>
+                        <Col lg={3} md={6} sm={6} onClick={()=>nav('/details')}>
                              <Card style={{ width: '100%' }}>
                             <Card.Img variant="top" src={value.img} />
                             <Card.Body>
